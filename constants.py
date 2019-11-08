210e42aaed581b8e21905d3c37eeb930b88fe081
Ubuntu <ubuntu@ip-172-31-17-81.ec2.internal>
2019-11-08 18:20:27 +0000

    added program

---
+++ constants.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Hearty
+# Created on: October 2019
+# This program Moves sprites on the PyBadge (with borders)
+
+# Circuitpython screen size is 160x120 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+NEW_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+
+# Using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
